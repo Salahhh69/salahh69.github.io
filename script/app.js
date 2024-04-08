65c9f6f85a790da663e681588af17cef2fd9ae1e
Salahhh69 <102703372+Salahhh69@users.noreply.github.com>
2024-04-08 22:53:03 +0000

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+
+const MAX_IMAGES = 6;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+
+});
+
+
+ function handleYesClick() {
+  titleElement.innerHTML = "Hehehe! :33";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+  window.open("../heart.html", "_self value");
+
+}
+  
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "M2ekda?",
+    "3afak",
+    "anmchi nbki fl9ent",
+    "guli ah u nchri lk lmakla",
+    "3yeqty jem3i rask",
+    "hhhhh sf yes mghtbqach tkber"
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
+
